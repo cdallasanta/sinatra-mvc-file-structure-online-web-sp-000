b32d6119ba762f299d2ee269eb80a56e601623fb
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-23 22:34:19 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,15 @@
 class Dog
+  attr_accessor :name, :breed, :age
+  @@all = []
+
+  def initialize(attr)
+    attr.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
